fa0994d08adbd3f9f6b28eb1c6e1d774a208a778
Thomas Tuts <thomas@darwinanalytics.com>
2014-06-26 08:56:16 +0200

    Use the new directory scheme in the card processor and dashboard

--- dashboard/src/controllers/overview.js
+++ dashboard/src/controllers/overview.js
@@ -1,7 +1,7 @@
 angular.module('hsDashboard')
   .controller('OverviewCtrl', function ($scope, $http) {
 
-    $http.get('/converted/1.0.0.5506.json').then(function (response) {
+    $http.get('/processed/1.0.0.5506/cards/1.0.0.5506.json').then(function (response) {
       $scope.cards = response.data;
     });
 
