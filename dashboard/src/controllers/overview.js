f41fd12466922818db1109f265dfe406f4bf8751
Thomas Tuts <thomas@darwinanalytics.com>
2014-06-27 16:07:38 +0200

    Add a texture processor

--- dashboard/src/controllers/overview.js
+++ dashboard/src/controllers/overview.js
@@ -1,7 +1,7 @@
 angular.module('hsDashboard')
   .controller('OverviewCtrl', function ($scope, $http) {
 
-    $http.get('/processed/1.0.0.5506/cards/1.0.0.5506.json').then(function (response) {
+    $http.get('/processed/1.0.0.5506/data/cards/cards.json').then(function (response) {
       $scope.cards = response.data;
     });
 
