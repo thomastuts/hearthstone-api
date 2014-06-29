8ab9a005acc922d26f639cfcd4eea9edcc91434e
Thomas Tuts <thomas@darwinanalytics.com>
2014-06-29 16:50:58 +0200

    Add scaffolding for extra card processors and testing

---
+++ test/lib/processors/text-filter.js
@@ -0,0 +1,18 @@
+var cardTexts = [
+  {
+    raw: '&amp;lt;b&amp;gt;Battlecry:&amp;lt;/b&amp;gt;Summon a 2/2 Pirate and destroy all Ninjas.',
+    filtered: 'Battlecry: Summon a 2/2 Pirate and destroy all Ninjas.'
+  },
+  {
+    raw: '&amp;lt;b&amp;gt;Hero Power&amp;lt;/b&amp;gt;\\n Summon a random Totem.',
+    filtered: 'Hero Power: Summon a random Totem.'
+  },
+  {
+    raw: '&amp;lt;b&amp;gt;Taunt&amp;lt;/b&amp;gt;',
+    filtered: 'Taunt'
+  },
+  {
+    raw: 'Draw a card. That card costs &amp;&#35;40;3&#41; less.',
+    filtered: 'Draw a card. That card costs 3 less.'
+  }
+];
